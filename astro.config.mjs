0cb0ac59b7c1b57933ed581c239584b974575b64
pressinghichamapp-maker <pressinghichamapp@gmail.com>
2025-11-03 18:49:28 +0100

    fix bug css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
-import purgecss from 'astro-purgecss';
 
 
 
@@ -15,7 +14,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ma-6zt.pages.dev/",
-  integrations: [react(), compress(), sitemap() , purgecss()],
+  integrations: [react(), compress(), sitemap()],
   adapter: cloudflare({ 
     imageService: "cloudflare"
   }),
