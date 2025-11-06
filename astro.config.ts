6ef8f6f076448b725379372c743791a3b78a2757
pressinghichamapp-maker <pressinghichamapp@gmail.com>
2025-11-06 19:05:09 +0100

    add unfonts

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import Unfonts from 'unplugin-fonts/astro'
 import cloudflare from "@astrojs/cloudflare";
 
 import tailwindcss from "@tailwindcss/vite";
@@ -14,7 +15,73 @@ import sitemap  from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://ma-6zt.pages.dev/",
   integrations: [react(), compress() ,
-
+    Unfonts({
+      // Custom fonts.
+      custom: {
+        /**
+         * Fonts families lists
+         */
+        families: [{
+          /**
+           * Name of the font family.
+           */
+          name: 'Poppins',
+          /**
+           * Local name of the font. Used to add `src: local()` to `@font-rule`.
+           */
+          local: 'Poppins',
+          /**
+           * Regex(es) of font files to import. The names of the files will
+           * predicate the `font-style` and `font-weight` values of the `@font-rule`'s.
+           */
+          src: '/Poppins/*.ttf',
+    
+          /**
+           * This function allow you to transform the font object before it is used
+           * to generate the `@font-rule` and head tags.
+           */
+          transform(font) {
+            if (font.basename === 'Poppins-Meduim') {
+              // update the font weight
+              font.weight = 600
+            }
+    
+            // we can also return null to skip the font
+            return font
+          }
+        }],
+    
+        /**
+         * Defines the default `font-display` value used for the generated
+         * `@font-rule` classes.
+         */
+        display: 'auto',
+    
+        /**
+         * Using `<link rel="preload">` will trigger a request for the WebFont
+         * early in the critical rendering path, without having to wait for the
+         * CSSOM to be created.
+         */
+        preload: true,
+    
+        /**
+         * Using `<link rel="prefetch">` is intended for prefetching resources
+         * that will be used in the next navigation/page load
+         * (e.g. when you go to the next page)
+         *
+         * Note: this can not be used with `preload`
+         */
+        prefetch: false,
+    
+        /**
+         * define where the font load tags should be inserted
+         * default: 'head-prepend'
+         *   values: 'head' | 'body' | 'head-prepend' | 'body-prepend'
+         */
+        injectTo: 'head-prepend',
+      },
+    
+    }),
     sitemap({
       // هنا كتحدد priority لكل صفحة
       serialize(item) {
