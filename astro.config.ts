de063df592fd9342eef4d0dcd8ac98b37dd8788b
pressinghichamapp-maker <pressinghichamapp@gmail.com>
2025-11-16 18:28:11 +0100

    add updates

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ import sitemap  from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.hostino.nl",
-  
+  publicDir: './public',
   integrations: [
     react(), 
     compress(),
