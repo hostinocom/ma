fc31e1b94b4c6a974abecba4cc25b4da52296fb6
pressinghichamapp-maker <pressinghichamapp@gmail.com>
2025-11-17 17:37:45 +0100

    edit

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
   
   // Redirect all requests to hostino.nl
   if (url.hostname !== 'www.hostino.nl') {
-    return Response.redirect(`https://www.hostino.nl${url.pathname}${url.search}`, 301);
+    return Response.redirect(`https://www.hostino.nl${url.pathname.toLocaleLowerCase()}${url.search.toLocaleLowerCase()}`, 301);
   }
   
   return next();
